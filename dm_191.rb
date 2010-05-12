0e137a0c5b09fb3e03cec2ccd9d421d8435e4a0f
John Mettraux <jmettraux@gmail.com>
2010-05-13 08:42:16 +0900

    forgot pass file for dm 1.9.1

--- dm_191.rb
+++ dm_191.rb
@@ -8,6 +8,8 @@ Ci::task do
   git 'http://github.com/jmettraux/ruote.git'
   git 'http://github.com/jmettraux/ruote-dm.git'
 
+  sh 'cp dm_connection.rb ruote-dm/test/integration_connection.rb'
+
   sh 'psql --version'
 
   options :dir => 'ruote', :rvm => '1.9.1@ruote_yajl'
